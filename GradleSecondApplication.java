5408d001c35b97ed288955cd0bcc28214ef43c1a
fengrui.yang <fengrui.yang@11bee.com>
2018-04-17 11:02:31 +0800

    aaa

---
+++ GradleSecondApplication.java
@@ -0,0 +1,12 @@
+package com.example.gradle.gradle_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleSecondApplication.class, args);
+    }
+}
